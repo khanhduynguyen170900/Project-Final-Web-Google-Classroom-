1bae72cb88e77e65eb8beeb4ff8c14190775236a
Antran <antran03031999@gmail.com>
2020-11-04 21:46:32 +0700

    Hello ae

--- Register.php
+++ Register.php
@@ -1,3 +1,7 @@
+
+<?php
+     require_once('connect.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,6 +22,63 @@
     <title >Đăng ký tài khoản</title>
 </head>
 <body class="register-form">
+
+<?php
+
+    $error = '';
+    $first_name = '';
+    $last_name = '';
+    $email = '';
+    $pass = '';
+    $pass_confirm = '';
+
+    if (isset($_POST['first']) && isset($_POST['last']) && isset($_POST['email'])
+    && isset($_POST['password']) && isset($_POST['passconfirm']))
+    {
+        $first_name = $_POST['first'];
+        $last_name = $_POST['last'];
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+        $pass_confirm = $_POST['passconfirm'];
+
+        if (empty($first_name)) {
+            $error = 'Please enter your first name';
+        }
+        else if (empty($last_name)) {
+            $error = 'Please enter your last name';
+        }
+        else if (empty($email)) {
+            $error = 'Please enter your email';
+        }
+        else if (filter_var($email, FILTER_VALIDATE_EMAIL) == false) {
+            $error = 'This is not a valid email address';
+        }
+        else if (empty($pass)) {
+            $error = 'Please enter your password';
+        }
+        else if (strlen($pass) < 6) {
+            $error = 'Password must have at least 6 characters';
+        }
+        else if ($pass != $pass_confirm) {
+            $error = 'Password does not match';
+        }
+        else{
+            $result = register($email,$pass,$first_name,$last_name);
+            if($result['code']==0)
+            {
+                header('Location:login.php');
+            }
+            else if($result['code']==1){
+                $error = 'This email is already exists';
+            }
+            else{
+                $error = 'An error occured. Please try again later';
+            }
+        }
+    }
+?>
+
+
     <div class="container">
         <div class="row">
             <div class="col-lg-3">
@@ -26,48 +87,39 @@
             <div class="col-lg-6">
                 <div id ="ui">
                     <h1 >Đăng ký tài khoản</h1>
-                    <form action="" class="form-group">
+                    <form action="" class="form-group" method="post">
                         <div class="form-group col-lg-6">
                             
                             <input value="" name="first" required class="form-control" type="text"  id="firstname">
-                            <label for="firstname">Họ</label>
+                            <label for="firstname">First Name</label>
                         </div>
                         <div class="form-group col-lg-6">
                             
                             <input value="" name="last" required class="form-control" type="text"  id="lastname">
-                            <label for="firstname">Tên</label>
+                            <label for="firstname">Last Name</label>
                         </div>
                         <div class="form-group col-lg-12" >
                             
                             <input value="" name="email" required class="form-control" type="email"  id="email">
                             <label for="email">Email</label>
                         </div>
-                        <div class="form-group col-lg-12" >
+                        
+                        <div class="form-group col-lg-6" >
                             
-                            <input value="" name="username" required class="form-control" type="text"  id="username">
-                            <label for="username">Tên tải khoản</label>
-                            
+                            <input value="" name="password" required class="form-control" type="password" id="pass">
+                            <label for="pass">Password</label>
                         </div>
                         <div class="form-group col-lg-6" >
                             
-                            <input value="" name="pass" required class="form-control" type="password" id="pass">
-                            <label for="pass">Mật khẩu</label>
+                            <input value="" name="passconfirm" required class="form-control" type="password"  id="pass">
+                            <label for="pass">Confirm Password</label>
                         </div>
-                        <div class="form-group col-lg-6" >
-                            
-                            <input value="" name="pass" required class="form-control" type="password"  id="pass">
-                            <label for="pass">Xác nhận mật khẩu</label>
-                        </div>
-                        <div class="form-group col-lg-12" >
-                           
-                            <input value="" name="phone" required class="form-control" type="number"  id="phone">
-                            <label for="phone">Số điện thoại</label>
-                        </div>
-                        <div class="form-group col-lg-12" >
-                            
-                            <input value="" name="birthdaytime" required   class="form-control" type="datetime"  id="birthdaytime"> 
-                            <label for="birthdaytime">Ngày sinh</label> 
-                        </div>
+
+                        <?php
+                            if (!empty($error)) {
+                                echo "<div style='color:red' class='alert'>$error</div>";
+                            }
+                        ?>
                         
 
                         <div class="form-group col-lg-6" >
